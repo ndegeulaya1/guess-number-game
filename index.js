1d4853b79d63c16299c35b2cea1e1b0089158a90
ndegeulaya <ndegeulayabaton@gmail.com>
2025-04-22 12:08:15 +0300

    updated

--- index.js
+++ index.js
@@ -6,12 +6,28 @@
 let secNumber = Math.trunc(Math.random()*20)+1;
 
 let decSore=20;
+let praying= true;
+
+function resert(){
+  decSore=20; 
+  secNumber = Math.trunc(Math.random()*20)+1;
+  document.querySelector('.message').textContent='👨‍🚀start guessing';
+  document.querySelector('.score').textContent=decSore;
+  document.querySelector('.number').textContent='?';
+
+  document.querySelector('body').style.backgroundColor='#000';
+  document.querySelector('.guess').value='';
+}
 const button= document.querySelector('.check');
 button.addEventListener('click', function(){
 
    
 const guess= Number(document.querySelector('.guess').value);
-    console.log(guess);
+
+
+
+  
+    
 
     //no input
     if(!guess){
@@ -19,11 +35,12 @@ const guess= Number(document.querySelector('.guess').value);
     }
     //correct guess
     else if(guess === secNumber){
-       console.log('am here');
+   
          document.querySelector('.message').textContent='👋 correct number';
          document.querySelector('.number').textContent=secNumber;
          document.querySelector('body').style.backgroundColor='#28a745';
-    let highScore=0;
+       let highScore=0;
+       playing=false;
          if(decSore>highScore){
             highScore=decSore;
             document.querySelector('.highScore').textContent=highScore;
@@ -33,29 +50,28 @@ const guess= Number(document.querySelector('.guess').value);
 
     //too hight and too low
   else if(guess!=secNumber){
+
+    
+  
     if(decSore>0){
         document.querySelector('.message').textContent= guess>secNumber? '😊 too hight' : '😁 too low'; 
         decSore = decSore-1;
         document.querySelector('.score').textContent=decSore;
+      
+
     }
     else{
         document.querySelector('.message').textContent='😊you lose';
         document.querySelector('body').style.backgroundColor='red';
     }
+    
+    document.querySelector('.number').textContent=secNumber;
+    secNumber = Math.trunc(Math.random()*20)+1;
   }
 
    
 });
 
    //resert the game
- document.querySelector('.again').addEventListener('click', function(){
-    decSore=20; 
-    secNumber = Math.trunc(Math.random()*20)+1;
-    document.querySelector('.message').textContent='👨‍🚀start guessing';
-    document.querySelector('.score').textContent=decSore;
-    document.querySelector('.number').textContent='?';
-
-    document.querySelector('body').style.backgroundColor='#000';
-    document.querySelector('.guess').value='';
-}
-)  ;
+ document.querySelector('.again').addEventListener('click', resert);
+   
